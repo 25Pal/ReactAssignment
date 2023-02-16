62b1a9ef09cd7fcef310565961ac17553aa2d067
25pal <pallaviuttekar333@gmail.com>
2023-02-16 13:04:59 +0530

    Card

---
+++ react3/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+import GetData from './Component/getData'
+
+function App() {
+  return (
+    <div >
+      <GetData/>
+      
+    </div>
+  );
+}
+
+export default App;
